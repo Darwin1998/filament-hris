547d38641cef1c98b4d66ea93b0ed72aaa6633df
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-08-15 09:55:15 +0800

    wip

--- app/Filament/Admin/Resources/EmployeeResource.php
+++ app/Filament/Admin/Resources/EmployeeResource.php
@@ -7,6 +7,7 @@ use Domain\Employee\Models\Employee;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -60,6 +61,11 @@ class EmployeeResource extends Resource
                                 'manager' => 'Manager',
                                 'designer' => 'Designer',
                             ]),
+                        SpatieMediaLibraryFileUpload::make('documents')
+                            ->disk('s3')
+                            ->multiple()
+                            ->previewable()
+                            ->collection('documents'),
                     ])->columns(2),
             ]);
     }
